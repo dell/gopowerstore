f9d5326396d577820328c54d56d19d53b388d42b
M <Shanmugapriya.M@Dell.com>
2022-05-02 08:14:36 +0530

    add mock function for GetSoftwareMajorMinorVersion API

--- mocks/Client.go
+++ mocks/Client.go
@@ -1466,6 +1466,27 @@ func (_m *Client) GetSoftwareInstalled(ctx context.Context) ([]gopowerstore.Soft
 	return r0, r1
 }
 
+// GetSoftwareMajorMinorVersion provides a mock function with given fields: ctx
+func (_m *Client) GetSoftwareMajorMinorVersion(ctx context.Context) (float32, error) {
+	ret := _m.Called(ctx)
+
+	var r0 float32
+	if rf, ok := ret.Get(0).(func(context.Context) float32); ok {
+		r0 = rf(ctx)
+	} else {
+		r0 = ret.Get(0).(float32)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetStorageISCSITargetAddresses provides a mock function with given fields: ctx
 func (_m *Client) GetStorageISCSITargetAddresses(ctx context.Context) ([]gopowerstore.IPPoolAddress, error) {
 	ret := _m.Called(ctx)
