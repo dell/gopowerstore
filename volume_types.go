423a41b280b752d92600d2b5d831da843465b289
Sakshi_makkar <Sakshi_makkar@dell.com>
2023-01-20 06:16:32 -0500

    comments addressed

--- volume_types.go
+++ volume_types.go
@@ -202,7 +202,7 @@ type VolumeModify struct {
 	//  Size must be a multiple of 8192.
 	Size int64 `json:"size,omitempty"`
 	// Unique identifier of the protection policy assigned to the volume.
-	ProtectionPolicyID string `json:"protection_policy_id"`
+	ProtectionPolicyID string `json:"protection_policy_id,omitempty"`
 	// Unique identifier of the performance policy assigned to the volume.
 	PerformancePolicyID string `json:"performance_policy_id,omitempty"`
 	// Description of the volume
