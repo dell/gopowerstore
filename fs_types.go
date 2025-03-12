60b8f4ddfe4dd6c263d2ab6d68daa16fe8bdc547
Rishabh Raj <rishabh.raj2@dell.com>
2025-03-12 05:44:48 -0500

    updated GetNASServers to return fs & health

--- fs_types.go
+++ fs_types.go
@@ -42,6 +42,20 @@ const (
 	FileSystemTypeEnumSnapshot FileSystemTypeEnum = "Snapshot" // Snapshot of a file system
 )
 
+// NASHealthState NAS health state
+type NASHealthState string
+
+const (
+	None     NASHealthState = "None"
+	Major    NASHealthState = "Major"
+	Minor    NASHealthState = "Minor"
+	Cricital NASHealthState = "Critical"
+)
+
+type HealthDetails struct {
+	State NASHealthState `json:"state,omitempty"`
+}
+
 type FLRCreate struct {
 	Mode             string `json:"mode,omitempty"`
 	MinimumRetention string `json:"minimum_retention,omitempty"`
@@ -258,6 +272,10 @@ type NAS struct {
 	CurrentPreferredIPv6InterfaceID string `json:"current_preferred_IPv6_interface_id,omitempty"`
 	// NfsServers define NFS server instance if nfs exports are present
 	NfsServers []NFSServerInstance `json:"nfs_servers"`
+	// FileSystems define file system instance that are present on the NAS server
+	FileSystems []FileSystem `json:"file_systems"`
+	// HealthDetails represent health details of the NAS server
+	HealthDetails HealthDetails `json:"health_details,omitempty"`
 	// PreferredNodeID represents the preferred node ID for the NAS server.
 	PreferredNodeID string `json:"preferred_node_id,omitempty"`
 	// DefaultUnixUser represents the default Unix user of the NAS server.
@@ -298,7 +316,7 @@ type NAS struct {
 
 // Fields returns fields which must be requested to fill struct
 func (n *NAS) Fields() []string {
-	return []string{"id", "description", "name", "current_node_id", "operational_status", "current_preferred_IPv4_interface_id", "current_preferred_IPv6_interface_id", "nfs_servers", "preferred_node_id", "default_unix_user", "default_windows_user", "current_unix_directory_service", "is_username_translation_enabled", "is_auto_user_mapping_enabled", "production_IPv4_interface_id", "production_IPv6_interface_id", "backup_IPv4_interface_id", "backup_IPv6_interface_id", "protection_policy_id", "file_events_publishing_mode", "is_replication_destination", "is_production_mode_enabled", "is_dr_test", "operational_status_l10n", "current_unix_directory_service_l10n", "file_events_publishing_mode_l10n"}
+	return []string{"id", "description", "name", "current_node_id", "operational_status", "current_preferred_IPv4_interface_id", "current_preferred_IPv6_interface_id", "nfs_servers", "file_systems", "health_details", "preferred_node_id", "default_unix_user", "default_windows_user", "current_unix_directory_service", "is_username_translation_enabled", "is_auto_user_mapping_enabled", "production_IPv4_interface_id", "production_IPv6_interface_id", "backup_IPv4_interface_id", "backup_IPv6_interface_id", "protection_policy_id", "file_events_publishing_mode", "is_replication_destination", "is_production_mode_enabled", "is_dr_test", "operational_status_l10n", "current_unix_directory_service_l10n", "file_events_publishing_mode_l10n"}
 }
 
 // Fields returns fields which must be requested to fill struct
