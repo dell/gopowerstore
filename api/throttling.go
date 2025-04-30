a2038edad3a89612d02274a6faa250f915a2502d
Bharath Sreekanth <bharath.sreekanth@emc.com>
2025-04-30 15:26:58 +0000

    refactor log statement

--- api/throttling.go
+++ api/throttling.go
@@ -58,12 +58,12 @@ func NewTimeoutSemaphore(timeout int64, rateLimit int, logger Logger) *TimeoutSe
 
 func (ts *TimeoutSemaphore) Acquire(ctx context.Context) error {
 	gpDeadline, _ := ctx.Deadline()
-	ts.Logger.Info(ctx, "acquire a lock deadline", gpDeadline)
+	ts.Logger.Info(ctx, "[Bharath] acquire a lock deadline", time.Until(gpDeadline))
 	var cancelFunc func()
 	ctx, cancelFunc = context.WithTimeout(ctx, ts.Timeout)
 
 	deadline, _ := ctx.Deadline()
-	ts.Logger.Info(ctx, "setting new context with deadline", deadline)
+	ts.Logger.Info(ctx, "[Bharath] setting new context with deadline", time.Until(deadline))
 
 	defer cancelFunc()
 	for {
